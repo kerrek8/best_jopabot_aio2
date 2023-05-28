4a6ffe2a7c1cebcae05490813c813aab792a4553
kerrek8 <kerrek8@gmail.com>
2023-05-28 22:18:47 +0700

    best jopabot v1.0.0

---
+++ config.py
@@ -0,0 +1,23 @@
+
+foto_dubai_id = 'AgACAgIAAxkBAAPrZEk8V7AwznPUujq57qv3pgOpppgAAmLJMRuyJklK3vbrcDHhqcUBAAMCAAN5AAMvBA'
+
+cats = [
+    'CgACAgIAAxkBAAIBDWRJ2yw-MYtpkLNiQe1HKuhsVQjqAAJqKwACsiZRSkJvysLxfg6OLwQ',
+    'CgACAgIAAxkBAAIBD2RJ2ysLJGBdAYf7pG7PAAF9DTsC5wACbSsAArImUUoBdKnA05Zz6i8E',
+    'CgACAgIAAxkBAAIBEWRJ21AWTm2wOQ9IgniWgk9A_JKLAAJuKwACsiZRSuBPRo5PekIzLwQ',
+    'CgACAgIAAxkBAAIBE2RJ201HKtfvcEbGHY1xOg00P0nxAAJvKwACsiZRSlWoB2w_1qIYLwQ'
+]
+
+transcript_dict = {
+    '\U00002649': 'taurus',
+    '\U00002648': 'aries',
+    '\U0000264E': 'libra',
+    '\U00002651': 'capricorn',
+    'taurus': 'Тельца',
+    'aries': 'Овна',
+    'libra': 'Весов',
+    'capricorn': 'Козерога',
+    'Сегодня': 'today',
+    'Завтра': 'tomorrow',
+    'Послезавтра': 'tomorrow02'
+}
